25bb5cc441955ba51db6b02c62bf68af8fc5f08a
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-08-27 22:36:43 +0300

    testing api

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ const randomBtn = document.querySelector(".randomize");
 const author = document.querySelector(".auth");
 const api_url = "https://api.quotable.io/random"
 
-async function getQuote() {
-    
+async function getQuote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
 }
+
+getQuote(api_url);
+
+
+randomBtn.addEventListener("click", () => {
+    getQuote(api_url);
+})
