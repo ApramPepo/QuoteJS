28de9036c9ccfa236a94556b417a73884566ebba
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-08-27 21:06:06 +0300

    design

---
+++ main.js
@@ -0,0 +1,8 @@
+const quote = document.querySelector(".quote");
+const randomBtn = document.querySelector(".randomize");
+const author = document.querySelector(".auth");
+const api_url = "https://api.quotable.io/random"
+
+async function getQuote() {
+    
+}
