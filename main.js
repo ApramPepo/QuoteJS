4fdf248361f72d207ae73db324bf3ccc340e55fc
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-08-27 23:22:45 +0300

    Finished and changed API

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const quote = document.querySelector(".quote");
 const randomBtn = document.querySelector(".randomize");
 const author = document.querySelector(".auth");
-const api_url = "https://api.quotable.io/random"
+const api_url = "https://thequoteshub.com/api/"
 
 async function getQuote(url) {
-    const response = await fetch(url);
-    var data = await response.json();
-    console.log(data);
+        const response = await fetch(url);
+        const data = await response.json();
+        quote.textContent = data.text;
+        author.textContent = data.author;
 }
 
 getQuote(api_url);
